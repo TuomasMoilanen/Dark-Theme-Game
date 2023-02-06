a59132dee2a2438e61a4293b15e9ee71300562fd
TuomasMoilanen <35306402+TuomasMoilanen@users.noreply.github.com>
2023-02-06 13:40:39 +0200

    Mostly working player animations. Bugs to be ironed out.

--- Assets/Scripts/Mechanics/Player/PlayerHealth.cs
+++ Assets/Scripts/Mechanics/Player/PlayerHealth.cs
@@ -7,12 +7,21 @@ public class PlayerHealth : MonoBehaviour
     public PlayerStats stats;
     public GameOver gameOver;
 
+    private Animator animator;
 
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
     public void TakeDamage(int damage)
     {
         stats.health -= damage;
         if (stats.health <= 0)
         {
+            animator.ResetTrigger("jumping");
+            animator.ResetTrigger("falling");
+            animator.ResetTrigger("walking");
+            animator.SetBool("isDead", true);
             gameOver.EndGame();
         }
 
--- Assets/Scripts/Mechanics/Player/PlayerMovement.cs
+++ Assets/Scripts/Mechanics/Player/PlayerMovement.cs
@@ -20,6 +20,8 @@ public class PlayerMovement : MonoBehaviour
     [Tooltip("Status if player is grounded.")]
     private bool isGrounded; // Status if player is grounded
 
+    private Animator animator;
+
     // ----------------------------------I   I---------------------------------- \\
 
     [Header("Jump / Fall multipliers")]
@@ -45,9 +47,17 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     [Tooltip("Creates Gizmo on unity to show the check layer.")]
     private LayerMask groundCheckLayer; // Creates Gizmo on unity to show the check layer
+    private Color groundCheckGizmoColor = new Color(0f, 0.5f, 0f, 0.5f); // Creates a color for the gizmo
 
     #endregion
 
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        // OnDrawGizmos(); // Draw a gizmo to show the ground check area
+
+    }
     void Update()
     {
         #region Ground Check
@@ -59,6 +69,8 @@ public class PlayerMovement : MonoBehaviour
         {
             isGrounded = false; // Player is not touching ground
         }
+
+
         #endregion
 
         #region Movement & Jump
@@ -77,10 +89,16 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetAxisRaw("Horizontal") < 0)
         {
             transform.rotation = Quaternion.Euler(0, 180, 0);
+            animator.SetTrigger("walking");
         }
         if (Input.GetAxisRaw("Horizontal") > 0)
         {
             transform.rotation = Quaternion.Euler(0, 0, 0);
+            animator.SetTrigger("walking");
+        }
+        if (Input.GetAxisRaw("Horizontal") == 0 || isGrounded == false)
+        {
+            animator.ResetTrigger("walking");
         }
 
         // Jump code --------------------------------------
@@ -88,16 +106,32 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetButtonDown("Jump") && isGrounded) // Gets keyboard input from 'unity input manager' and translates it into players transform attributes,
         {                                              // to make player model move up and down
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            animator.SetTrigger("jumping");
         }
         if (rb.velocity.y < 0)
         {
             rb.velocity += Vector2.up * Physics.gravity * (fallMultiplier - 1) * Time.deltaTime;
+            animator.ResetTrigger("jumping");
+            animator.SetTrigger("falling");
 
         }
         else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
         {
             rb.velocity += Vector2.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+            animator.SetTrigger("jumping");
+        }
+        if (isGrounded == true)
+        {
+            animator.ResetTrigger("jumping");
+            animator.ResetTrigger("falling");
         }
     }
+    // private void OnDrawGizmos()
+    // {
+
+    //     Gizmos.color = groundCheckGizmoColor;
+
+    //     Gizmos.DrawSphere(groundCheckPosition.position, groundCheckRadius); // Draws a gizmo for the groundCheck area
+    // }
     #endregion
 }
